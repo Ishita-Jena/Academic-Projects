ab578be99e42d8832677322ad38cbe15312af031
Ishita-Jena <167574361+Ishita-Jena@users.noreply.github.com>
2025-01-15 18:06:34 +0530

    Add files via upload

---
+++ Tic-Tac-Toc/Code/Server.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(2000);
+        Socket socket = null;
+        while (true) {
+            System.out.println("waiting client 1...........");
+            socket = serverSocket.accept();
+            Client c1 = new Client(socket);
+            System.out.println("client 1 joined..........");
+            System.out.println("waiting client 2...........");
+            socket = serverSocket.accept();
+            Client c2 = new Client(socket);
+            System.out.println("client 2 joined..........");
+            GameEngine ge = new GameEngine(c1, c2);
+            ge.run();
+        }
+    }
+}
